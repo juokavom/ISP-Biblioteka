911d6e528b0082cee03d7e92bd4a5e18f2370e1a
Jokubas Akramas <j.akramas@gmail.com>
2020-11-06 02:52:22 +0200

    Nukreipimai į vartotojo duomenų redagavimo langą iš vartotojo panelės (self) ir vartotojų list (admin)

--- ISP-Biblioteka/Controllers/UserController.cs
+++ ISP-Biblioteka/Controllers/UserController.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ISP_Biblioteka.Models;
-using System.Data;
-using System.Configuration;
-using MySql.Data.MySqlClient;
 
 namespace ISP_Biblioteka.Controllers
 {
@@ -15,27 +11,13 @@ namespace ISP_Biblioteka.Controllers
         // GET: User
         public ActionResult Index()
         {
-            /*List<User> list1 = new List<User>();
-            string mainconn = ConfigurationManager.ConnectionStrings["Mysqlconnection"].ConnectionString;
-            MySqlConnection mysql = new MySqlConnection(mainconn);
-            string query = "select * from test";
-            MySqlCommand comm = new MySqlCommand(query);
-            comm.Connection = mysql;
-            mysql.Open();
-            MySqlDataReader dr = comm.ExecuteReader();
-            while (dr.Read())
-            {
-                list1.Add(new Models.User 
-                {
-                    Name = dr["name"].ToString(),
-                    Id = Int32.Parse(dr["id"].ToString()),
-                    ImagePath = "~/Image/User/" + dr["image"].ToString()
-                });
-            }
-            mysql.Close();*/
-
-           // return View(list1);
             return View();
+        }
+        public ActionResult Edit(string email, bool admin)
+        {
+            Models.User user = new Models.User { Email = email};
+            user.updateValues();
+            return View(user);
         }
     }
 }
