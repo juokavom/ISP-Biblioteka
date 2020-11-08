676f352dcc1b60f455f237cfdcefa65e6989dc17
EurobeatR26B <justas.gabulas@ktu.edu>
2020-11-08 19:24:19 +0200

    Pridėtas inventoriaus registravimo langas

---
+++ ISP-Biblioteka/Models/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISP_Biblioteka.Models
+{
+    public class Location
+    {
+        string zone { get; set; }
+        int id { get; set; }
+        int shelfNumber { get; set; }
+        int shelfRow { get; set; }
+
+        public Location(string zoneName, int zoneID, int shelfNumber, int shelfRow)
+        {
+            this.zone = zoneName;
+            this.id = zoneID;
+            this.shelfNumber = shelfNumber;
+            this.shelfRow = shelfRow;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Zona {zone} (ID {id}), lentyna {shelfNumber}, eilė {shelfRow}");
+        }
+    }
+}
