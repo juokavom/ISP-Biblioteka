c75914c336ad6f04a7ffb1964dbc4339612023ce
Descory <simas0315@gmail.com>
2020-11-08 02:29:35 +0200

    Aktyvus uzsakymai, laiskas, knygos prasymas

---
+++ ISP-Biblioteka/Controllers/NewsLetterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISP_Biblioteka.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+        // GET: NewsLetter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ISP-Biblioteka/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISP_Biblioteka.Models;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Reflection;
+using Microsoft.Ajax.Utilities;
+
+namespace ISP_Biblioteka.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public DateTime Borrow_date { get; set; }
+        public DateTime Return_date { get; set; }
+        public DateTime Validation_date { get; set; }
+
+        public Order()
+        {
+
+        }
+    }
+}
