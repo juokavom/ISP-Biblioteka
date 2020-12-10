6ade9d3578dae2ef40b191fbb89553502b806ecc
paulius9902 <55972634+paulius9902@users.noreply.github.com>
2020-12-10 19:02:13 +0200

    -

--- ISP-Biblioteka/Controllers/AtaskaitaController.cs
+++ ISP-Biblioteka/Controllers/AtaskaitaController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ISP_Biblioteka.Repos;
+using ISP_Biblioteka.ViewModels;
 
 namespace ISP_Biblioteka.Controllers
 {
     public class AtaskaitaController : Controller
     {
+        NeaktyvusVartotojaiRepository neaktyvRepository = new NeaktyvusVartotojaiRepository();
+
         // GET: Ataskaita
         public ActionResult UzsakymuIstorija()
         {
@@ -31,7 +35,22 @@ namespace ISP_Biblioteka.Controllers
         }
         public ActionResult NeaktyvusVartotojai()
         {
-            return View();
+            NeaktyvusViewModel neaktyvus = new NeaktyvusViewModel();
+
+            PopulateSelections(neaktyvus);
+
+            neaktyvus.neaktyvus = neaktyvRepository.getNeaktyvusVartotojai();
+
+            return View(neaktyvus);
+        }
+
+        public void PopulateSelections(NeaktyvusViewModel vart)
+        {
+            List<SelectListItem> selectListlaikotarpiai = new List<SelectListItem>();
+
+            selectListlaikotarpiai.Add(new SelectListItem() { Value = Convert.ToString(1), Text = "1 mėnuo" });
+
+            vart.LaikotarpisList = selectListlaikotarpiai;
         }
     }
 }
---
+++ ISP-Biblioteka/ViewModels/NeaktyvusViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISP_Biblioteka.ViewModels
+{
+    public class NeaktyvusViewModel
+    {
+        public List<NeaktyvusVartotojaiViewModel> neaktyvus { get; set; }
+        public int laikotarpis { get; set; }
+        public IList<SelectListItem> LaikotarpisList { get; set; }
+    }
+}
